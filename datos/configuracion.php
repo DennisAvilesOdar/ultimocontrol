bc32f13023047006d104f3a4ac74dcc5d103b096
Dennis Aviles Odar <dennis.aviles@icloud.com>
2016-11-20 08:33:49 -0500

    .

---
+++ datos/configuracion.php
@@ -0,0 +1,18 @@
+<?php
+
+
+define("SERVIDOR_BD", "ec2-54-243-201-144.compute-1.amazonaws.com");
+define("USUARIO_BD", "gxlsdzbloqtjkp");
+define("CLAVE_BD", "QMM4ljWevGz2Al4TOVDJcFDGt8");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "de4jpgclq8751j");
+ 
+
+/*
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "postgres");
+define("CLAVE_BD", "USAT2016");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "bdcomercial_ws");
+ */
+ 
